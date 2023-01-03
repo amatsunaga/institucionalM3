2146b9edc67c6f2260ba6ac3a47776f2d3d3c8ab
Andrea Matsunaga <andreamizuno@gmail.com>
2023-01-03 17:31:37 -0300

    feat: cria breadcrumbs

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -4,9 +4,16 @@ import { AsideMenu } from "../AsideMenu/AsideMenu";
 
 import styles from "./MainLayout.module.scss";
 
+// import "../../assets/svg/Vector.svg";
+
 const MainLayout = () => {
   return (
     <main className={styles["main-container"]}>
+      <div className={styles["breadcrumbs"]}>
+        <div className={styles["home-icon"]}></div>
+        <div className={styles["arrow-icon"]}></div>
+        <p className={styles["current-location"]}>Institucional</p>
+      </div>
       <h1 className={styles["main-title"]}>Institucional</h1>
       <div className={styles["main-content"]}>
         <AsideMenu />
