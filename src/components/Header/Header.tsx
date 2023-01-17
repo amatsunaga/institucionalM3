e4d73b8f6f3d970a002a5bccadcadb01510d983c
Andrea Matsunaga <andreamizuno@gmail.com>
2023-01-17 15:41:16 -0300

    refactor(header): renomeia HeaderBottom para HeaderNavbar, aplica WidthContext no header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,25 @@
-// import React from "react";
-
-import styles from "./Header.module.scss";
+import React, { useContext } from "react";
 
 import { HeaderDesktop } from "./HeaderDesktop/HeaderDesktop";
 import { HeaderMobile } from "./HeaderMobile/HeaderMobile";
+import { WidthContext } from "../../contexts/WidthContext";
+
+import styles from "./Header.module.scss";
 
 const Header = () => {
+  const { isMobile } = useContext(WidthContext);
+
   return (
     <>
-      <HeaderMobile className={`${styles["container"]} ${styles["header-mobile"]}`} />
-      <HeaderDesktop className={`${styles["container"]} ${styles["header-desktop"]}`} />
+      {isMobile ? (
+        <HeaderMobile
+          className={`${styles["container"]} ${styles["header-mobile"]}`}
+        />
+      ) : (
+        <HeaderDesktop
+          className={`${styles["container"]} ${styles["header-desktop"]}`}
+        />
+      )}
     </>
   );
 };
